d3bbd0b6c56b981d4d97e20bd990718aab894f32
steven-yn <steven_yoon1009@naver.com>
2023-10-21 16:11:02 +0900

    add: 우선순위 큐 구현

--- src/app.ts
+++ src/app.ts
@@ -1,109 +1,130 @@
-class MaxBinaryHeap {
-  values: number[];
-  constructor() {
-    this.values = [];
+class PriorityNode {
+  value: any;
+  priority: number;
+  constructor(value: any, priority: number) {
+    this.value = value;
+    this.priority = priority;
   }
+}
 
-  getChildIndex(index: number, direction: "LEFT" | "RIGHT") {
-    if (direction === "LEFT") return 2 * index + 1;
-    return 2 * index + 2;
+class PriorityQueue {
+  heap: PriorityNode[];
+  constructor() {
+    this.heap = [];
   }
 
   getParentIndex(index: number) {
     return Math.floor((index - 1) / 2);
   }
 
-  insert(value: number) {
-    this.values.push(value);
-    this.bubbleUp(this.values.length - 1);
-
-    return this;
+  getChildIndex(index: number, mode: "LEFT" | "RIGHT") {
+    if (mode === "LEFT") return 2 * index + 1;
+    return 2 * index + 2;
   }
 
-  bubbleUp(newIndex: number) {
-    if (newIndex === 0) return;
+  enqueue(value: any, priority: number) {
+    this.heap.push(new PriorityNode(value, priority));
+    this.bubbleUp(this.heap.length - 1);
+    return this.heap;
+  }
 
-    let parentIndex = this.getParentIndex(newIndex);
+  bubbleUp(index: number) {
+    if (index === 0) return;
 
-    const newValue = this.values[newIndex];
-    const parentValue = this.values[parentIndex];
+    const parentIndex = this.getParentIndex(index);
+    const targetNode = this.heap[index];
 
-    if (newValue > parentValue) {
-      this.values[parentIndex] = newValue;
-      this.values[newIndex] = parentValue;
+    if (this.heap[index].priority < this.heap[parentIndex].priority) {
+      this.heap[index] = this.heap[parentIndex];
+      this.heap[parentIndex] = targetNode;
       this.bubbleUp(parentIndex);
     }
 
-    return;
+    return targetNode;
   }
 
-  remove() {
-    const originRoot = this.values[0];
-    const lastValue = this.values[this.values.length - 1];
-    this.values[0] = lastValue;
-    this.values.pop();
-
-    this.bubbleDown(0);
+  dequeue(test?: boolean) {
+    const originRoot = this.heap[0];
+    this.heap[0] = this.heap[this.heap.length - 1];
+    this.heap[this.heap.length - 1] = originRoot;
+    this.heap.pop();
+    this.bubbleDown(0, test);
 
     return originRoot;
   }
 
-  bubbleDown(newIndex: number) {
-    const targetValue = this.values[newIndex];
-    const leftChildIndex = this.getChildIndex(newIndex, "LEFT");
-    const rightChildIndex = this.getChildIndex(newIndex, "RIGHT");
+  bubbleDown(index: number, test?: boolean) {
+    const currentNode = this.heap[index];
 
-    if (leftChildIndex === 0 || rightChildIndex === 0) return;
+    if (this.heap.length <= 1) return currentNode;
+    if (this.heap.length - 1 === index) return currentNode;
 
-    const leftChild = this.values[leftChildIndex];
-    const rightChild = this.values[rightChildIndex];
+    const leftChildIndex = this.getChildIndex(index, "LEFT");
+    const rightChildIndex = this.getChildIndex(index, "RIGHT");
 
-    if (
-      this.values[newIndex] < leftChild &&
-      this.values[newIndex] < rightChild
-    ) {
-      if (leftChild > rightChild) {
-        this.values[newIndex] = leftChild;
-        this.values[leftChildIndex] = targetValue;
+    const leftNode = this.heap[leftChildIndex];
+    const rightNode = this.heap[rightChildIndex];
 
-        this.bubbleDown(leftChildIndex);
-      } else if (rightChild > leftChild) {
-        this.values[newIndex] = rightChild;
-        this.values[rightChildIndex] = targetValue;
-
-        this.bubbleDown(rightChildIndex);
-      }
-    } else if (this.values[newIndex] < leftChild) {
-      this.values[newIndex] = leftChild;
-      this.values[leftChildIndex] = targetValue;
+    const leftSwap = () => {
+      this.heap[index] = leftNode;
+      this.heap[leftChildIndex] = currentNode;
 
       this.bubbleDown(leftChildIndex);
-    } else if (this.values[newIndex] < rightChild) {
-      this.values[newIndex] = rightChild;
-      this.values[rightChildIndex] = targetValue;
+    };
+
+    const rightSwap = () => {
+      this.heap[index] = rightNode;
+      this.heap[rightChildIndex] = currentNode;
 
       this.bubbleDown(rightChildIndex);
+    };
+
+    if (rightChildIndex >= this.heap.length) {
+      if (leftNode && leftNode.priority < currentNode.priority) leftSwap();
+
+      return currentNode;
     }
 
-    return targetValue;
+    if (
+      leftNode.priority < currentNode.priority &&
+      rightNode.priority < currentNode.priority
+    ) {
+      if (leftNode.priority < rightNode.priority) {
+        leftSwap();
+      } else if (rightNode.priority < leftNode.priority) {
+        rightSwap();
+      } else {
+        leftSwap();
+      }
+    } else if (leftNode.priority < currentNode.priority) {
+      leftSwap();
+    } else if (rightNode.priority < currentNode.priority) {
+      rightSwap();
+    } else {
+      leftSwap();
+    }
   }
 }
 
-const heap = new MaxBinaryHeap();
-heap.insert(41);
-heap.insert(39);
-heap.insert(33);
-heap.insert(18);
-heap.insert(27);
-heap.insert(12);
-heap.insert(55);
-console.log(heap);
-console.log(heap.remove(), "extract");
-console.log(heap.remove(), "extract");
-console.log(heap.remove(), "extract");
-console.log(heap.remove(), "extract");
-console.log(heap.remove(), "extract");
-console.log(heap.remove(), "extract");
-console.log(heap.remove(), "extract");
-console.log(heap.remove(), "extract");
-console.log(heap);
+const queue = new PriorityQueue();
+
+queue.enqueue("common cold", 5);
+queue.enqueue("glass in foot", 3);
+queue.enqueue("gunshot wound", 1);
+queue.enqueue("glass in foot", 3);
+queue.enqueue("high fever", 4);
+queue.enqueue("glass in foot", 3);
+queue.enqueue("broken arm", 2);
+queue.enqueue("glass in foot", 3);
+
+console.log(queue.heap);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.heap);
